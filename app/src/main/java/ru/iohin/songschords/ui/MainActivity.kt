014a38c66d2af55ba99c39a85287e3207096eaac
Pavel Iohin <pavel@iohin.ru>
2023-03-22 16:19:23 +0300

    setup basic navigation component, created blank search fragment

---
+++ app/src/main/java/ru/iohin/songschords/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.iohin.songschords.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.iohin.songschords.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ feature-search/src/main/java/ru/iohin/songschords/feature_search/ui/SearchViewModel.kt
@@ -0,0 +1,7 @@
+package ru.iohin.songschords.feature_search.ui
+
+import androidx.lifecycle.ViewModel
+
+class SearchViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
