1418f2660332cfd3f40bc1d9ae30b1706e4dfe50
Pavel Iohin <pavel@iohin.ru>
2023-06-14 10:22:23 +0200

    highlighting chords bold in song text

--- feature/song/src/main/java/ru/iohin/songschords/feature/song/ui/Song.kt
+++ feature/song/src/main/java/ru/iohin/songschords/feature/song/ui/Song.kt
@@ -1,17 +1,29 @@
 package ru.iohin.songschords.feature.song.ui
 
+import android.text.Html
+import android.text.Spannable
+import android.text.SpannableString
 import ru.iohin.songschords.core.api.entity.SongFull
 
 data class Song(
     val name: String,
     val artistName: String,
-    val content: String
+    val content: Spannable
 ) {
     companion object {
-        fun from(songFull: SongFull) = Song(
-            songFull.name,
-            songFull.artistName,
-            songFull.content.replace("[crd]", "").replace("[/crd]", "")
-        )
+        fun from(songFull: SongFull): Song {
+            val htmlString = songFull.content
+                .replace("[crd]", "<b>")
+                .replace("[/crd]", "</b>")
+                .replace("\n", "<br>")
+                .replace(" ", "&nbsp;")
+            val html = Html.fromHtml(htmlString)
+            val spannableString = SpannableString(html)
+            return Song(
+                songFull.name,
+                songFull.artistName,
+                spannableString
+            )
+        }
     }
 }
